713b07f48bc36ba45358419df85ee7ce8539b55c
John Yeary <john.yeary@infor.com>
2013-12-10 15:17:41 -0500

    Updated the RealRequestProxy to use the ServletContext.getResource in the case of resources being embedded in an EAR, or WAR file. The previous method would throw an NPE on JS/CSS that was not in an exploded EAR/WAR file.

--- tag-main/src/main/java/com/granule/RealRequestProxy.java
+++ tag-main/src/main/java/com/granule/RealRequestProxy.java
@@ -1,12 +1,13 @@
 /*
  * Copyright 2010 Granule Inc.
- * 
+ * Copyright 2013 Blue Lotus Software, LLC.
+ *
  * Licensed under the Apache License, Version 2.0 (the "License"); you may not
  * use this file except in compliance with the License. You may obtain a copy of
  * the License at
- * 
+ *
  * http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
  * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
@@ -15,40 +16,64 @@
  */
 package com.granule;
 
+import com.granule.logging.Logger;
+import com.granule.logging.LoggerFactory;
+import java.io.File;
+import java.net.MalformedURLException;
+import java.net.URISyntaxException;
 import javax.servlet.http.HttpServletRequest;
 
 /**
- * User: Dario Wunsch
- * Date: 20.07.2010
- * Time: 2:35:48
+ * @author Dario Wunsch
+ * @author John Yeary <jyeary@bluelotussoftware.com>
+ * @version 1.1
  */
 public class RealRequestProxy implements IRequestProxy {
-    private HttpServletRequest request;
+
+    private final HttpServletRequest request;
+    private static final Logger logger = LoggerFactory.getLogger(RealRequestProxy.class);
 
     public RealRequestProxy(HttpServletRequest request) {
         this.request = request;
     }
 
+    @Override
     public String getRealPath(String path) {
-        return request.getSession().getServletContext().getRealPath(path);
+        if (path != null && !path.startsWith("/")) {
+            path = "/" + path;
+        }
+        File f = null;
+        try {
+            f = new File(request.getServletContext().getResource(path).toURI());
+        } catch (MalformedURLException ex) {
+            logger.error("The URL for the path is malformed.", ex);
+        } catch (URISyntaxException ex) {
+            logger.error("The URI has a syntax issue.", ex);
+        }
+        return f != null ? f.getAbsolutePath() : null;
     }
 
+    @Override
     public Object getAttribute(String name) {
         return request.getAttribute(name);
     }
 
+    @Override
     public void setAttribute(String name, Object o) {
         request.setAttribute(name, o);
     }
 
+    @Override
     public String getServletPath() {
         return request.getServletPath();
     }
 
+    @Override
     public String getBasePath() {
         return request.getContextPath();
     }
-    
+
+    @Override
     public String getContextPath() {
         return request.getContextPath();
     }
