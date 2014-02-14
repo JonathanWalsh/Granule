a51626846d729130a6761306367bf2c3fd981622
John Yeary <johnyeary@gmail.com>
2014-02-14 16:35:42 -0500

    Fixed version reporting issue in CompressServlet, and moved config.properties to /META-INF

--- tag-main/src/main/java/com/granule/CompressServlet.java
+++ tag-main/src/main/java/com/granule/CompressServlet.java
@@ -1,12 +1,12 @@
 /*
  * Copyright 2010 Granule Inc.
- * 
+ *
  * Licensed under the Apache License, Version 2.0 (the "License"); you may not
  * use this file except in compliance with the License. You may obtain a copy of
  * the License at
- * 
+ *
  * http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
  * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
@@ -19,7 +19,6 @@ import com.granule.cache.TagCacheFactory;
 import com.granule.logging.Logger;
 import com.granule.logging.LoggerFactory;
 import com.granule.utils.PathUtils;
-
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -33,20 +32,19 @@ import java.util.logging.Level;
 import java.util.logging.LogRecord;
 
 /**
- * User: Dario Wunsch
- * Date: 21.06.2010
- * Time: 2:04:08
+ * Servlet Responsible for compression.
+ *
+ * @author Dario Wunsch
+ * @author John Yeary
+ * @version 1.11.1
  */
 public class CompressServlet extends HttpServlet {
 
-    private static final long serialVersionUID = -2526640346318371192L;
-
+    private static final long serialVersionUID = -371092409070142705L;
+    private static final Logger logger = LoggerFactory.getLogger(CompressServlet.class);
     private static final String CLOSURE_COMPILER_PACKAGE_NAME = "com.google.javascript.jscomp";
-
     private static final String ID_PARAMETER = "id";
-
     private static final String VERSION_KEY = "version";
-
     private static String version = "unknown";
 
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException,
@@ -80,40 +78,38 @@ public class CompressServlet extends HttpServlet {
             throw new ServletException(e);
         }
 
-    
         logger.info(MessageFormat.format("Granule {0} Started", version));
     }
-    
-	private void loadVersion() {
-		Properties props = new Properties();
-		try {
-			InputStream resourceAsStream = PathUtils.getResourceAsStream(this
-					.getClass(), "/com/granule/config.properties");
-			try {
-				if (resourceAsStream == null)
-					logger
-							.warn("Can not find /com/granule/config.properties resource");
-				else
-					props.load(resourceAsStream);
-			} finally {
-				if (resourceAsStream != null)
-					resourceAsStream.close();
-			}
-			if (props.containsKey(VERSION_KEY))
-				version = props.getProperty(VERSION_KEY);
-		} catch (IOException e) {
-			logger.warn("Can not load config.properties", e);
-		}
-	}
 
-    public class CompilerLogFilter implements Filter {
-        public boolean isLoggable(LogRecord lr) {
-            return !(lr.getSourceClassName().startsWith(CLOSURE_COMPILER_PACKAGE_NAME) && lr.getLevel().intValue() <
-                    Level.WARNING.intValue());
+    private void loadVersion() {
+        Properties props = new Properties();
+        try {
+            InputStream resourceAsStream = PathUtils.getResourceAsStream(this.getClass(), "/META-INF/config.properties");
+            try {
+                if (resourceAsStream == null) {
+                    logger.warn("Can not find /META-INF/config.properties resource");
+                } else {
+                    props.load(resourceAsStream);
+                }
+            } finally {
+                if (resourceAsStream != null) {
+                    resourceAsStream.close();
+                }
+            }
+            if (props.containsKey(VERSION_KEY)) {
+                version = props.getProperty(VERSION_KEY);
+            }
+        } catch (IOException e) {
+            logger.warn("Can not load config.properties", e);
         }
     }
 
+    public class CompilerLogFilter implements Filter {
 
-    private static final Logger logger = LoggerFactory.getLogger(CompressServlet.class);
+        @Override
+        public boolean isLoggable(LogRecord lr) {
+            return !(lr.getSourceClassName().startsWith(CLOSURE_COMPILER_PACKAGE_NAME) && lr.getLevel().intValue()
+                    < Level.WARNING.intValue());
+        }
+    }
 }
-
